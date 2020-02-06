e3128eaf56037f6ae5f722674a681267902d8678
isaac c <isaacgc0596@gmail.com>
2020-02-06 05:01:37 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -71,6 +71,7 @@ def gross_per_studio(collection)
   end
   return result
 end
+
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
