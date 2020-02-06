244a9fe207f43d3516b4e7e3f00302dad33606e8
isaac c <isaacgc0596@gmail.com>
2020-02-06 04:14:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -87,7 +87,7 @@ def movies_with_directors_set(source)
   while a_index < source.length do
     d_name = source[a_index][:name]
     m_movie = source[a_index][:movies]
-    d_movies << movies_with_director_key(d_name, m_movie)
+    d_movies << 
     a_index += 1
     end
     binding.pry
